f981a9dd556628fb45624b83c43fbec542da58d0
F74036386 <F74036386@mail.ncku.edu.tw>
2017-05-31 16:54:38 +0800

    questionInfoRequire.php v0.3

--- questionInfoAcquire.php
+++ questionInfoAcquire.php
@@ -1,61 +1,65 @@
 <?php
 
-function timeAcquire(&$input,&$ans,&$info){
-	if($info["time"]!=""){
+function timeAcquire(&$input,&$data,&$ans,&$info){
+	if($info["time"][0]==""){
 	 $ans="請問您說的時間點是甚麼時候?";
-	 	$info["time"]="waitReplay";
+	 	$info["time"][0]="waitReplay";
 
 		return 0;
 	}
-	else if($info["time"]=="waitReplay"){
-		$info["time"]=$input;
+	else if($info["time"][0]=="waitReplay"){
+		$info["time"][0]=$input;
 		$info["amount"]++;
+		$info["amount"]["time"]++;
 		return 1;
 	}
 	else return 1;
 
 }
 
-function placeAcquire(&$input,&$ans,&$info){
-	if($info["place"]!=""){
+function placeAcquire(&$input,&$data,&$ans,&$info){
+	if($info["place"][0]==""){
 	 $ans="請問您說的地點是哪裡?";
-	 $info["place"]="waitReplay";
+	 $info["place"][0]="waitReplay";
 
 		return 0;
 	}
-	else if($info["place"]=="waitReplay"){
-		$info["place"]=$input;
+	else if($info["place"][0]=="waitReplay"){
+		$info["place"][0]=$input;
 		$info["amount"]++;
+		$info["amount"]["place"]++;
 		return 1;
 	}
 		else return 1;
 
 }
 
-function eventAcquire(&$input,&$ans,&$info){
-	if($info["event"]!=""){
+function eventAcquire(&$input,&$data,&$ans,&$info){
+	if($info["event"][0]==""){
 	 $ans="請問您是發生了甚麼事?";
-	 $info["event"]="waitReplay";
+	 $info["event"][0]="waitReplay";
 
 		return 0;
 	}
-	else if($info["event"]=="waitReplay"){
-		$info["event"]=$input;
+	else if($info["event"][0]=="waitReplay"){
+		$info["event"][0]=$input;
 		$info["amount"]++;
+		$info["amount"]["event"]++;
 		return 1;
 	}
 	else return 1;
 }
 
-function objectAcquire(&$input,&$ans,&$info){
-	if($info["obj"]!=""){
+function objectAcquire(&$input,&$data,&$ans,&$info){
+	if($info["obj"][0]==""){
 	 $ans="請問您說的他是誰?";
-	 $info["obj"]="waitReplay";
+	 $info["obj"][0]="waitReplay";
 
 		return 0;
 	}
-	else if($info["obj"]=="waitReplay"){
-		$info["obj"]=$input;
+	else if($info["obj"][0]=="waitReplay"){
+		$info["obj"][0]=$input;
+		$info["amount"][obj]++;
 		$info["amount"]++;
 		return 1;
 	}
